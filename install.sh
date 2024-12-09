fd6289577f0bf1df3cadd13fbfd2ab0d7f915bb5
Touko Nurminen <touko.nurminen@aidian.eu>
2024-12-09 09:10:20 +0200

    Add nvim config as a submodule

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@
 
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/nvim ~/.config/nvim
+
+echo "Symbolic links created:"
+echo "  - ~/.zshrc -> ~/.dotfiles/.zshrc"
+echo "  - ~/.tmux.conf -> ~/.dotfiles/.tmux.conf"
+echo "  - ~/.config/nvim -> ~/.dotfiles/nvim"
