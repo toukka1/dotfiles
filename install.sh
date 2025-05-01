8c0ca2b86261afcd3fc07c5d1fc6d827d3902c4e
Touko Nurminen <touko.nurminen@gmail.com>
2025-05-01 21:09:51 +0300

    cray

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/nvim ~/.config/nvim
-
-echo "Symbolic links created:"
-echo "  - ~/.zshrc -> ~/.dotfiles/.zshrc"
-echo "  - ~/.tmux.conf -> ~/.dotfiles/.tmux.conf"
-echo "  - ~/.config/nvim -> ~/.dotfiles/nvim"
+ln -sf ~/.dotfiles/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sf ~/.dotfiles/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+ln -sf ~/.dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf ~/.dotfiles/waybar/waybarconfig ~/.config/waybar/config
+ln -sf ~/.dotfiles/waybar/waybarstyle.css ~/.config/waybar/style.css
