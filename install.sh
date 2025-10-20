fbf72865fa36cb1d9c7229a11355503934eea40c
Touko Nurminen <touko.nurminen@aidian.eu>
2025-10-20 14:26:13 +0300

    Improve search functions

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
-ln -sf ~/.dotfiles/zsh/zprofile ~/.zprofile
-ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-mkdir -p ~/.config/nvim && ln -sf ~/.dotfiles/nvim ~/.config/nvim
-mkdir -p ~/.config/git && ln -sf ~/.dotfiles/git/config ~/.config/git/config
-mkdir -p ~/.config/git && ln -sf ~/.dotfiles/git/ignore ~/.config/git/ignore
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zsh/zprofile ~/.zprofile
+ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/nvim && ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+mkdir -p ~/.config/git && ln -sf ~/dotfiles/git/config ~/.config/git/config
+mkdir -p ~/.config/git && ln -sf ~/dotfiles/git/ignore ~/.config/git/ignore
