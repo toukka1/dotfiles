a09f241dc6230428fd47b4caa7fae72944a73656
Touko Nurminen <touko.nurminen@gmail.com>
2025-10-29 20:32:24 +0200

    Waybar improvements, hyprshot, hyprlock, file manager

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/nvim && ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
 mkdir -p ~/.config/hypr && ln -sf ~/dotfiles/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
 mkdir -p ~/.config/hypr && ln -sf ~/dotfiles/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+mkdir -p ~/.config/hypr && ln -sf ~/dotfiles/hyprland/hyprlock.conf ~/.config/hypr/hyprlock.conf
 mkdir -p ~/.config/kitty && ln -sf ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
 mkdir -p ~/.config/waybar && ln -sf ~/dotfiles/waybar/waybarconfig ~/.config/waybar/config
 mkdir -p ~/.config/waybar && ln -sf ~/dotfiles/waybar/waybarstyle.css ~/.config/waybar/style.css
